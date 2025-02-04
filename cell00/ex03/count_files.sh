3cfbd2ee51428bf98a4a5034efe4e9f172caff08
pomneii <pomneii75@gmail.com>
2025-02-04 11:19:17 +0700

    update ex03

--- cell00/ex03/count_files.sh
+++ cell00/ex03/count_files.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Count regular files and directories (excluding hidden ones)
-count=$(find . -maxdepth 1 -type f -o -type d ! -name '.' | wc -l)
+files=($(ls))
 
-# Display the count only, no $ at the end
-echo "$count"
+echo "${#files[@]}$" 
